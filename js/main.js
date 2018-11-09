06f61395636abcb0ac7078717dea4986810a012f
Lea Loncke <lealoncke1204@gmail.com>
2018-11-09 13:55:00 +0100

    Finished exercise

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 
+let container = document.getElementById('productsContainer');
+
+function Product(name, phrase, description) {
+  // Create elements
+  this.div =  document.createElement('div');
+  this.name = document.createElement('h1');
+  this.phrase = document.createElement('h2');
+  this.description = document.createElement('p');
+  // Add elements in the DOM
+  container.appendChild(this.div);
+  this.div.appendChild(this.name);
+  this.div.appendChild(this.phrase);
+  this.div.appendChild(this.description);
+  // Add content in elements
+  this.name.innerHTML = name;
+  this.phrase.innerHTML = phrase;
+  this.description.innerHTML = description;
+}
+
+let HTMLManager = {
+  name: document.getElementById('name'),
+  phrase: document.getElementById('phrase'),
+  description: document.getElementById('description'),
+  displayAndReset: function() {
+    let newProduct = new Product(this.name.value, this.phrase.value, this.description.value);
+    this.name.value = "";
+    this.phrase.value = "";
+    this.description.value = "";
+  }
+}
+
+
 let submit = document.getElementById('submit');
 
 submit.addEventListener("click", function() {
-  console.log('Hello, World !');
+  HTMLManager.displayAndReset();
 })
+
 
 
 
